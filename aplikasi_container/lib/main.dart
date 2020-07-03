f98a2416c6910457e09ec6a92e677bdbede7ed5a
drwngwn <67698663+drwngwn@users.noreply.github.com>
2020-07-03 20:40:47 +0700

    Add files via upload

---
+++ aplikasi_container/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Latihan Container"),
+        ),
+        body: Container(
+          color: Colors.red,
+          margin: EdgeInsets.all(10),
+          child: Container(
+            color: Colors.blue,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
